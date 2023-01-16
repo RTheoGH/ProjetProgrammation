6740fc6db0b54cad8644a38787df34bff4e86c63
RTheoGH <reynier.theo@gmail.com>
2023-01-16 15:52:30 +0100

    bon

--- serveur.py
+++ serveur.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projet.db'
-db.init_app(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projet.db'
+# db.init_app(app)
 
-class Question(db.Model):
-    idQ = db.Column(db.Integer, primary_key=True)
-    enonce = db.Column(db.String(300), nullable=False)
-    reponses = db.Column(db.String(200),db.ForeignKey('reponse.idR'), nullable=False)
+# class Question(db.Model):
+#     idQ = db.Column(db.Integer, primary_key=True)
+#     enonce = db.Column(db.String(300), nullable=False)
+#     reponses = db.Column(db.String(200),db.ForeignKey('reponse.idR'), nullable=False)
 
-    def __constructeur(u):
-        return 'Question %r'% u.idQ
+#     def __constructeur(u):
+#         return 'Question %r'% u.idQ
 
 # class Reponse(db.Model):
 #     idR = db.Column(db.Integer,primary_key=True)
@@ -21,8 +21,8 @@ class Question(db.Model):
 #     def __constructeur(u):
 #         return 'Reponse %r'% u.idR
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 questions=[]
 
@@ -34,16 +34,16 @@ def index():
 def ajout():
     if request.method == 'POST':
         question = request.form['question']
-        new_question = Question(enonce=question)
-
-        try:
-            db.session.add(new_question)
-            db.session.commit()
-            return redirect('/lquestion')
-        except:
-            return 'erreur'
-        # reponse = request.form['reponse']
-        # return redirect(url_for('lquestion'))
+        # new_question = Question(enonce=question)
+        # try:
+        #     db.session.add(new_question)
+        #     db.session.commit()
+        #     return redirect('/lquestion')
+        # except:
+        #     return 'erreur'
+        reponse = request.form['reponse']
+        questions.append({"question":question,"reponse":reponse})
+        return redirect(url_for('lquestion'))
     else:
         return render_template("ajoutQuestion.html")
 
