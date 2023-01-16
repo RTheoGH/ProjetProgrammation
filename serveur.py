8c54a07a7cd64f4b860e60f12cec86411c6c62b5
RTheoGH <reynier.theo@gmail.com>
2023-01-16 15:30:50 +0100

    a

--- serveur.py
+++ serveur.py
@@ -8,18 +8,18 @@ db.init_app(app)
 
 class Question(db.Model):
     idQ = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(300), nullable=False)
+    enonce = db.Column(db.String(300), nullable=False)
     reponses = db.Column(db.String(200),db.ForeignKey('reponse.idR'), nullable=False)
 
     def __constructeur(u):
         return 'Question %r'% u.idQ
 
-class Reponse(db.Model):
-    idR = db.Column(db.Integer,primary_key=True)
-    reponse = db.Column(db.String(200), nullable=False)
+# class Reponse(db.Model):
+#     idR = db.Column(db.Integer,primary_key=True)
+#     reponse = db.Column(db.String(200), nullable=False)
 
-    def __constructeur(u):
-        return 'Reponse %r'% u.idR
+#     def __constructeur(u):
+#         return 'Reponse %r'% u.idR
 
 with app.app_context():
     db.create_all()
@@ -34,9 +34,16 @@ def index():
 def ajout():
     if request.method == 'POST':
         question = request.form['question']
-        reponse = request.form['reponse']
-        questions.append({"question":question,"reponse":reponse})
-        return redirect(url_for('lquestion'))
+        new_question = Question(enonce=question)
+
+        try:
+            db.session.add(new_question)
+            db.session.commit()
+            return redirect('/lquestion')
+        except:
+            return 'erreur'
+        # reponse = request.form['reponse']
+        # return redirect(url_for('lquestion'))
     else:
         return render_template("ajoutQuestion.html")
 
