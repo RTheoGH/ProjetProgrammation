09445597eb529688cebd4c6643134f28c15671b8
RTheoGH <reynier.theo@gmail.com>
2023-01-16 10:41:46 +0100

    code de base

--- serveur.py
+++ serveur.py
@@ -2,20 +2,24 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+questions=[]
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/ajout",methods = ['POST', 'GET'])
-def login():
+def ajout():
     if request.method == 'POST':
-        return redirect(url_for('stock'))
+        question = request.form['question']
+        questions.append(question)
+        return redirect(url_for('lquestion'))
     else:
-        return render_template("ajout.html")
+        return render_template("ajoutQuestion.html")
 
-@app.route("/stock",methods = ['GET'])
-def stock():
-    return render_template("stock.html")
+@app.route("/lquestion",methods = ['GET'])
+def lquestion():
+    return render_template("lquestion.html",lquestion=questions)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
