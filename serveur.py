3651fcf6e57df2f26e2bdcd97261964e6d569d07
RTheoGH <reynier.theo@gmail.com>
2023-01-16 14:09:31 +0100

    readme + serveur

--- serveur.py
+++ serveur.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
+
+# class baseQ(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     question = db.Column(db.String(300), nullable=False)
+#     reponses = db.Column(db.String(200), nullable=False)
+
+#     def __repr__(self):
+#         return '<Question %r' % self.id
+
 
 questions=[]
 
@@ -21,6 +33,11 @@ def ajout():
 @app.route("/lquestion",methods = ['GET'])
 def lquestion():
     return render_template("lquestion.html",lquestion=questions)
+
+@app.route("/supprimer")
+def modifier():
+    questions.remove()
+    return redirect(url_for("lquestion"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
