26fb3a93f635381fbc73f905ad120b2b30128ef6
RTheoGH <reynier.theo@gmail.com>
2023-03-13 14:03:05 +0100

    légère modif qcm

--- serveur.py
+++ serveur.py
@@ -454,18 +454,8 @@ def supprimer(id):
         return 'Erreur lors de la suppression de la question'
         #Renvoi message d'erreur si échec de la suppression de la question
 
-@app.route("/listeQCM",methods = ['POST','GET'])           #Route pour créer un qcm à partir des questions crée par l'utilisateur
-def qcm():
-    title='Création QCM'
-    if 'nomU' not in session:       #Sécurité connexion
-        flash("Connectez vous ou créer un compte pour accéder à cette page")
-        return redirect(url_for('index'))
-    LQ = db.session.query(Question).filter(Question.idU==session['idU']).all()  #Récupération questions de la base de données
-    print(LQ)
-    return render_template("QCM.html",title=title,ListesQuestions=LQ,page="CréerQcm")
-
-@app.route("/generateQCM",methods = ['GET','POST'])      #Route qui genere le qcm
-def generate():
+@app.route("/listeQCM",methods = ['GET','POST'])      #Route qui genere le qcm
+def listeQCM():
     title='Vos QCM'
     if 'nomU' not in session:                   #Sécurité connexion
         flash("Connectez vous ou créer un compte pour accéder à cette page")
